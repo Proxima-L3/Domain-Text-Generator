579ed54d0a613e4bc679863af66ae7989c2d2948
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-05-25 19:00:32 -0700

    Changed the markov chain text generator function to use probability to more closely be modeled as a markov chain. Also added a conditional in case user chooses starter word that doesnt exist in the corpora

--- scratch_paper.py
+++ scratch_paper.py
@@ -4,6 +4,7 @@ Because I don't exactly know how the end product will be organized and this is a
 """
 
 import numpy as np
+import random
 from collections import defaultdict
 
 
@@ -38,19 +39,22 @@ corpora_markov_chain_map = create_markov_chain_map(corpora_list)
 
 def markov_chain_text_gen(corpora_chain_map, input_starter_word, output_text_length):
 
-    inner_dict = corpora_chain_map[input_starter_word]
-    next_word = 'The'
+    next_word = ''
     output_text_list = []
     output_text = ''
 
+    if input_starter_word in corpora_chain_map:
+        inner_dict = corpora_chain_map[input_starter_word]
+    else:
+        inner_dict = corpora_chain_map['The']
+
     while len(output_text_list) < output_text_length:
 
-        # come up with next word in output text by finding the word that follows most frequently
-        word_instances = 0
-        for possible_word in inner_dict:
-            if inner_dict[possible_word] > word_instances:
-                next_word = possible_word
-                word_instances = inner_dict[possible_word]
+        # if next word inner dictionary is not empty, come up with next word in output text via randomizer weighted by which options follow most frequently
+        if inner_dict:
+            key_list = list(inner_dict.keys())
+            value_list = list(inner_dict.values())
+            next_word = str(random.choices(key_list, value_list, k=1)[0])
         
         # focus next code loop to decided upon next word
         inner_dict = corpora_chain_map[next_word]
