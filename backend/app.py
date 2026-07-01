82da4add3ee4c8ecf05fe8c54e1fcee19b7bd28e
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-07-01 14:46:51 -0700

    Added try and except input validation to app.py routes

--- backend/app.py
+++ backend/app.py
@@ -8,22 +8,33 @@ CORS(app, origins=['http://localhost:5174', 'https://proxima-l3.github.io'])
 
 @app.route('/generate', methods=['POST'])
 def index():
-    user_input_topic = request.json['topic']
-    user_input_catalyst = request.json['catalyst']
-    user_input_text_length = int(request.json['textLength'])
+    try:
+        user_input_topic = request.json['topic']
+        user_input_catalyst = request.json['catalyst']
+        user_input_text_length = int(request.json['textLength'])
 
-    generated_text_output = main(user_input_topic, user_input_catalyst, user_input_text_length)
+        generated_text_output = main(user_input_topic, user_input_catalyst, user_input_text_length)
+    except ValueError:
+        return jsonify({'error': 'invalid number format for text length!'}), 400
+    except KeyError:
+        return jsonify({'error': 'invalid topic and/or catalyst!'}), 400
 
     return jsonify({'generated_text': generated_text_output})
 
 @app.route('/api/generate', methods=['POST'])
 def api_generate():
-    specialization = request.json['specialization']
-    input_text_length = int(request.json['word_count'])
-
+    
     specialization_topic_catalyst_map = {'medical - experimental autogen text': ['cryonics', 'Cryogenic preservation']}
 
-    input_topic, input_catalyst = specialization_topic_catalyst_map[specialization]
+    try:
+        specialization = request.json['specialization']
+        input_text_length = int(request.json['word_count'])
+
+        input_topic, input_catalyst = specialization_topic_catalyst_map[specialization]
+    except ValueError:
+        return jsonify({'error': 'invalid number format for word_count'}), 400
+    except KeyError:
+        return jsonify({'error': 'invalid specialization!'}), 400
 
     generated_text_output = main(input_topic, input_catalyst, input_text_length)
 
