e5631c12fa3f7f136242539306d1f89588c196f9
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-07-21 18:58:05 -0700

    Added a build_models.py cli tool module that will help initialize and update markov chain text generator models into couchdb for later use (some serious reorganization of code will need to take place)

---
+++ backend/build_models.py
@@ -0,0 +1,12 @@
+import couchdb
+
+# server = couchdb.Server('http://localhost:5984/')
+# db = server['your_database_name']
+
+# # save a document
+# db.save({'_id': 'carpentry', 'model': serialized_model})
+
+# # load a document
+# doc = db['carpentry']
+
+
