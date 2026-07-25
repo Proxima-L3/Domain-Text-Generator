6994f3cc6dcbaa9ba178ef83109e66013d801718
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-07-25 14:47:22 -0700

    Updated function and module doc strings to properly reflect all the changes made to each function and the module as a whole

--- backend/build_models.py
+++ backend/build_models.py
@@ -1,6 +1,17 @@
+"""Contains three functions: one for creating markov chain models, one for saving new models to couchdb database, and another for loading previously saved models from the database.
+
+This module is the part of the pipeline responsible for creating, saving, and loading markov chain models from couchdb database. It can also be ran as a cli command tool by entering "python build_models.py --all" while in the backend directory's virtual environment to build all saved specialized fields in the specialization_map in constants.py or if only one new field needs to be saved, "python build_models.py --field "specialization model name"".
+
+Functions:
+    create_serialized_model: This function actually creates the markov chain model by accepting an api class, user_input_topic, and corpora_count as parameters and using those to run majority of the pipeline that retrieves the corpora, sets it to a string, processes/cleans it, trains the markov chain model on the corpora, and serializes it before returning the newly made corpora markov chain model.
+    save_model_to_db: This function is responsible for updating or saving newly serialized markov chain models to the environment's defined couchdb database url.
+    load_model_from_db: This function looks up markov chain models in the database before deserializing them and returning them as an output.
+"""
+
+
 import argparse
 from constants import db, specialization_map
-from corpora_retrieval import gutendex_api, pmc_api, mediawiki_api
+# from corpora_retrieval import gutendex_api, pmc_api, mediawiki_api
 from src.preprocess import process_api_retrieved_corpora_to_string, clean_up_corpora_string
 from src.train import create_markov_chain_map
 
@@ -11,7 +22,7 @@ def create_serialized_model(corpora_api_class, user_input_topic, corpora_count):
     Creates the corpora markov chain map model and outputs it so that it can be saved to the couchdb by the save_model function.
     """
     # call corpora processor and pass input topic and article count as arguments
-    corpora_set_string = process_api_retrieved_corpora_to_string(corpora_api_class,user_input_topic, corpora_count)
+    corpora_set_string = process_api_retrieved_corpora_to_string(corpora_api_class, user_input_topic, corpora_count)
 
     # cleans up corpora string, adds punctuation tokenization, and adds sentence start tag indicators to help markov chain determine when sentences should begin
     clean_corpora_string = clean_up_corpora_string(corpora_set_string)
@@ -25,9 +36,9 @@ def create_serialized_model(corpora_api_class, user_input_topic, corpora_count):
     return serialized_corpora_markov_chain_map
 
 def save_model_to_db(specialization, corpora_api_class, user_input_topic, corpora_count):
-    """
+    """Calls the function that creates the markov chain model and saves it to couchdb database.
     
-    
+    This function expects parameters: specialization, a corpora api class to pull corpora from, a user input topic which is conditionally used by corpora classes in different ways, and the corpora count which defines how many articles/corpora source text to use to train model. It then runs the create_serialized_model function to create the markov chain model and if the specialization/model name exists in the database, it updates it with the newer version of the trained model. If it doesn't exist yet, it makes a new entry with the specialization name as the _id and the serialized markov chain model as the model key's value.
     """
     
     serialized_markov_chain_model = create_serialized_model(corpora_api_class, user_input_topic, corpora_count)
@@ -41,9 +52,9 @@ def save_model_to_db(specialization, corpora_api_class, user_input_topic, corpor
         db.save({'_id': specialization, 'model': serialized_markov_chain_model})
 
 def load_model_from_db(specialization):
-    """
+    """Uses the specialization argument to load a serialized model from couchdb, then deserializes it, and returns the model.
     
-    
+    This function expects only one parameter: specialization field. This single parameter is used to look up and retrieve the corresponding serialized markov chain model. It then deserializes it (because couchdb doesn't support saving tuples), then returns the deserialized markov chain model.
     """
     
     # load a document (or rather a markov chain model from database)
